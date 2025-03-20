2cd1a447fb60dc38ed146d36edb6ee352def8c4e
Santiago Galiano <santiago.galiano@ua.es>
2025-03-20 13:59:17 +0100

    Modify entrypoint to execute evaluation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 wandb login $WANDB_API_KEY  # Authenticate W&B
 
 # Run tasks sequentially
-#./execAllScripts.sh $MODEL_ID_HUGGING_FACE  $WANDB_PROJECT
+./execAllScripts.sh $MODEL_ID_HUGGING_FACE  $WANDB_PROJECT
 python3 format_results.py
 
 # Keep the container alive
